b8a4327cf8fa22891b069454fe3ab11034227fc8
Arif Akkermans <arif.akkermans@vodafoneziggo.com>
2021-12-10 15:07:17 +0100

    created a new TypeScript CDK project

---
+++ test/api-cors-lambda-crud-dynamodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApiCorsLambdaCrudDynamodb from '../lib/api-cors-lambda-crud-dynamodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/api-cors-lambda-crud-dynamodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApiCorsLambdaCrudDynamodb.ApiCorsLambdaCrudDynamodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
