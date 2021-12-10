367a819b3b8fd847aabf53bb6306f6ca3fddb7c6
Arif Akkermans <arif.akkermans@vodafoneziggo.com>
2021-12-10 15:48:27 +0100

    update api gateway methods

--- lib/api-cors-lambda-crud-dynamodb-stack.ts
+++ lib/api-cors-lambda-crud-dynamodb-stack.ts
@@ -82,5 +82,45 @@ export class ApiCorsLambdaCrudDynamodbStack extends Stack {
     const api = new RestApi(this, 'bookApi', {
       restApiName: 'Book API Service'
     });
+
+    const items = api.root.addResource('books');
+    items.addMethod('GET', listBookIntegration);
+    items.addMethod('POST', createBookIntegration);
+    addCorsOptions(items);
+
+    const singleItem = items.addResource('{id}');
+    singleItem.addMethod('GET', getBookIntegration);
+    singleItem.addMethod('PATCH', updateBookIntegration);
+    singleItem.addMethod('DELETE', deleteBookIntegration);
+    addCorsOptions(singleItem);
+
   }
+}
+ // Add CORS to restrict cross-origin HTTP requests
+export function addCorsOptions(apiResource: IResource) {
+  apiResource.addMethod('OPTIONS', new MockIntegration({
+    integrationResponses: [{
+      statusCode: '200',
+      responseParameters: {
+        'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+        'method.response.header.Access-Control-Allow-Origin': "'*'",
+        'method.response.header.Access-Control-Allow-Credentials': "'false'",
+        'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
+      },
+    }],
+    passthroughBehavior: PassthroughBehavior.NEVER,
+    requestTemplates: {
+      "application/json": "{\"statusCode\": 200}"
+    },
+  }), {
+    methodResponses: [{
+      statusCode: '200',
+      responseParameters: {
+        'method.response.header.Access-Control-Allow-Headers': true,
+        'method.response.header.Access-Control-Allow-Methods': true,
+        'method.response.header.Access-Control-Allow-Credentials': true,
+        'method.response.header.Access-Control-Allow-Origin': true,
+      },
+    }]
+  })
 }
