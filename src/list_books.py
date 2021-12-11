36da7724a6eb62854d1e50b586b8f0ff61c7377b
Arif Akkermans <arif.akkermans@vodafoneziggo.com>
2021-12-11 11:31:07 +0100

    add get all function

--- src/list_books.py
+++ src/list_books.py
@@ -14,8 +14,24 @@ dynamodb_client = boto3.client('dynamodb')
 
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+    logger.info(f'Incoming request is: {event}')
+
+    # Set the default error response
+    response = {
+        "statusCode": 500,
+        "body": "An error occured while fetching all books."
     }
+
+    scan_result = dynamodb_client.scan(TableName=TABLE_NAME)['Items']
+
+    books = []
+
+    for item in scan_result:
+        books.append(item)
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(books)
+    }
+
+    return response
