2b0dad44052c75c0ae7042a5174160120fa9d5b1
Arif Akkermans <arif.akkermans@vodafoneziggo.com>
2021-12-13 17:43:49 +0100

    create book request body dataclass

--- src/models.py
+++ src/models.py
@@ -5,6 +5,9 @@ Models and helper functions.
 import json
 import logging
 
+from dataclasses import dataclass
+from typing import Any, TypeVar, Type, cast
+
 
 class InvalidUsage(Exception):
     """
@@ -119,3 +122,58 @@ class DynamoParser:
                 for item in value:
                     data.append(self.__unmarshal_value(item))
                 return data
+
+
+T = TypeVar("T")
+
+
+def from_str(x: Any) -> str:
+    assert isinstance(x, str)
+    return x
+
+
+def to_class(c: Type[T], x: Any) -> dict:
+    assert isinstance(x, c)
+    return cast(Any, x).to_dict()
+
+
+@dataclass
+class BookRequestBody:
+    name: str
+    isbn: str
+    authors: str
+    languages: str
+    countries: str
+    number_of_pages: int
+    release_date: str
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'BookRequestBody':
+        assert isinstance(obj, dict)
+        name = from_str(obj.get("name"))
+        isbn = from_str(obj.get("isbn"))
+        authors = from_str(obj.get("authors"))
+        languages = from_str(obj.get("languages"))
+        countries = from_str(obj.get("countries"))
+        number_of_pages = from_str(obj.get("numberOfPages"))
+        release_date = from_str(obj.get("releaseDate"))
+        return BookRequestBody(name, isbn, authors, languages, countries, number_of_pages, release_date)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["name"] = from_str(self.name)
+        result["isbn"] = from_str(self.isbn)
+        result["authors"] = from_str(self.authors)
+        result["languages"] = from_str(self.languages)
+        result["countries"] = from_str(self.countries)
+        result["numberOfPages"] = from_str(str(self.number_of_pages))
+        result["releaseDate"] = from_str(self.release_date)
+        return result
+
+
+def book_request_body_from_dict(s: Any) -> BookRequestBody:
+    return BookRequestBody.from_dict(s)
+
+
+def book_request_body_to_dict(x: BookRequestBody) -> Any:
+    return to_class(BookRequestBody, x)
